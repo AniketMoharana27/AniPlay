e285e73e50cf73a4419aa73f6767d3fb71732981
AniketMoharana27 <aniketmoharana87@gmail.com>
2022-01-24 20:13:32 +0530

    AniPlay

---
+++ data.js
@@ -0,0 +1,99 @@
+let songs = [
+{
+    name: 'Kill em with Kindness',
+    path: './music/song 1.mp3',
+    artist: 'Selena Gomez',
+    cover: 'images/cover 1.jpg'
+},
+
+{
+    name: 'Drag me down',
+    path: 'music/song 2.mp3',
+    artist: 'One Direction',
+    cover: 'images/cover 2.jpg'
+},
+
+{
+    name: 'Shape of you',
+    path: 'music/song 3.mp3',
+    artist: 'Ed Sheeran',
+    cover: 'images/cover 3.jpg'
+},
+
+{
+    name: 'Heat Waves',
+    path: 'music/song 4.mp3',
+    artist: 'Glass Animal',
+    cover: 'images/cover 4.jpg'
+},
+
+{
+    name: 'Better',
+    path: 'music/song 5.mp3',
+    artist: 'Khalid',
+    cover: 'images/cover 5.jpg'
+},
+
+{
+    name: 'Lie To Me',
+    path: 'music/song 6.mp3',
+    artist: '5 Seconds of Summer',
+    cover: 'images/cover 6.jpg'
+},
+
+{
+    name: 'Animals',
+    path: 'music/song 7.mp3',
+    artist: 'Martin Garrix',
+    cover: 'images/cover 7.jpg'
+},
+
+{
+    name: 'Levitating',
+    path: 'music/song 8.mp3',
+    artist: 'Dua Lipa',
+    cover: 'images/cover 8.jpg'
+},
+
+{
+    name: 'Yummy',
+    path: 'music/song 9.mp3',
+    artist: 'Justin Bieber',
+    cover: 'images/cover 9.jpg'
+},
+
+{
+    name: 'Girls Like You',
+    path: 'music/song 10.mp3',
+    artist: 'Maroon 5',
+    cover: 'images/cover 10.jpg'
+},
+
+{
+    name: 'Better Now',
+    path: 'music/song 11.mp3',
+    artist: 'Post Malone',
+    cover: 'images/cover 11.jpg'
+},
+
+{
+    name: 'Blinding Lights',
+    path: 'music/song 12.mp3',
+    artist: 'The Weeknd',
+    cover: 'images/cover 12.jpg'
+},
+
+{
+    name: 'Sathi',
+    path: 'music/song 13.mp3',
+    artist: 'Ritviz & Nuclei',
+    cover: 'images/cover 13.jpg'
+},
+
+{
+    name: 'Excuses',
+    path: 'music/song 14.mp3',
+    artist: 'AP Dhillon',
+    cover: 'images/cover 14.jpg'
+}
+];
